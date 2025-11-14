e09ed0b57c861ec34a9962261a4b339ffb13318f
arkar1022 <arkar1022@gmail.com>
2025-11-14 18:20:49 +0700

    Fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    serverComponentsExternalPackages: ['sweph'],
     outputFileTracingIncludes: {
       '/api/natal': [
         './public/ephe/**/*',
-        './node_modules/sweph/build/Release/sweph.node'
+        './node_modules/sweph/build/**/*'
       ]
     }
   }
